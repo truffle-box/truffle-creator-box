2ef0400affe6b37634710854e898a5555228313c
MB <951378+michaeljohnbennett@users.noreply.github.com>
2022-02-10 15:45:30 +1100

    Fixing layouts .
    Wiring in wagmi login connector.

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
-import { Navbar, Section } from 'react-bulma-components';
-import styled from 'styled-components';
+import { Navbar } from "react-bulma-components";
+import styled from "styled-components";
 
-import truffle from "../Assets/icon.png"
+import Login from "./Login";
 
-// import Login from "./Torus"
-
-import {
-  Link
-} from "react-router-dom";
+import truffle from "../Assets/icon.png";
+import { Link } from "react-router-dom";
 
 const StyledLink = styled(Link)`
   color: #4a4a4a;
@@ -19,45 +16,40 @@ const Navigation = () => {
   const [navIsActive, setNavIsActive] = useState(false);
 
   return (
-      <Navbar
-        active={navIsActive}
-        transparent={true}
-        style={{background: "none"}}
-      >
-        <Navbar.Brand>
+    <Navbar
+      active={navIsActive}
+      transparent={true}
+      style={{ background: "none" }}
+    >
+      <Navbar.Brand>
+        <Navbar.Item renderAs="span">
+          <Link to="/">
+            <img src={truffle} alt="Truffle Creator" />
+          </Link>
+        </Navbar.Item>
+        <Navbar.Burger
+          onClick={() => setNavIsActive(navIsActive ? false : true)}
+        />
+      </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Container>
           <Navbar.Item renderAs="span">
-            <Link to="/">
-              <img src={truffle} alt="Truffle Creator" />
-            </Link>
+            <StyledLink to="/">home</StyledLink>
           </Navbar.Item>
-          <Navbar.Burger onClick={() => setNavIsActive(navIsActive ? false : true) }/>
-        </Navbar.Brand>
-        <Navbar.Menu>
-          <Navbar.Container>
-            <Navbar.Item renderAs="span">
-              <StyledLink to="/">
-                home
-              </StyledLink>
-            </Navbar.Item>
-            <Navbar.Item renderAs="span">
-              <StyledLink to="/gallery">
-                gallery
-              </StyledLink>
-            </Navbar.Item>
-            <Navbar.Item renderAs="span">
-              <StyledLink to="/about">
-                about
-              </StyledLink>
-            </Navbar.Item>
-          </Navbar.Container>
-          <Navbar.Container align="end">
-            <Navbar.Item href="#">
-              Login
-              {/* <Login accountDialog={accountDialog} showAccountDialog={showAccountDialog} metadataCID={metadataCID} /> */}
-            </Navbar.Item>
-          </Navbar.Container>
-        </Navbar.Menu>
-      </Navbar>
+          <Navbar.Item renderAs="span">
+            <StyledLink to="/gallery">gallery</StyledLink>
+          </Navbar.Item>
+          <Navbar.Item renderAs="span">
+            <StyledLink to="/about">about</StyledLink>
+          </Navbar.Item>
+        </Navbar.Container>
+        <Navbar.Container align="end">
+          <Navbar.Item renderAs="span">
+            <Login />
+          </Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
+    </Navbar>
   );
 };
 
