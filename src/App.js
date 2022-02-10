80be25274c70805da460238a65232cce9fbda4ab
MB <951378+michaeljohnbennett@users.noreply.github.com>
2022-02-10 17:32:55 +1100

    Revert "Fixing layouts ."
    This reverts commit 2ef0400affe6b37634710854e898a5555228313c.

--- src/App.js
+++ src/App.js
@@ -8,46 +8,8 @@ import Navigation from "./Components/Navigation";
 import { Routes, Route, Outlet} from "react-router-dom";
 import Gallery from "./Pages/Gallery";
 
-import { Provider, chain, defaultChains } from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-// import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-// import { WalletLinkConnector } from 'wagmi/connectors/walletLink'
-
-// API key for Ethereum node via infura
-const infuraId = process.env.INFURA_ID
-
-// Chains for connectors to support
-const chains = defaultChains
-
-// Set up connectors
-const connectors = ({ chainId }) => {
-  const rpcUrl =
-    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
-    chain.mainnet.rpcUrls[0]
-  return [
-    new InjectedConnector({
-      chains,
-      options: { shimDisconnect: true },
-    }),
-    // new WalletConnectConnector({
-    //   options: {
-    //     infuraId,
-    //     qrcode: true,
-    //   },
-    // }),
-    // new WalletLinkConnector({
-    //   options: {
-    //     appName: 'My wagmi app',
-    //     jsonRpcUrl: `${rpcUrl}/${infuraId}`,
-    //   },
-    // }),
-  ]
-}
-
-
 function App() {
   return (
-    <Provider autoConnect connectors={connectors}>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<AppPage />} />
@@ -57,7 +19,6 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-    </Provider>
   );
 }
 
