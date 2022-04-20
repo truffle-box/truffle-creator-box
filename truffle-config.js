f8ec778222ed30a29df304f410edb38cc1dc34a0
Kevin Bluer <kevin@bluer.com>
2022-04-20 16:51:12 -0500

    tidying config

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,4 @@
-const wrapProvider = require('arb-ethers-web3-bridge').wrapProvider
-const HDWalletProvider = require('@truffle/hdwallet-provider')
 
-const mnemonic = '';
-
-var path = require('path');
 
 module.exports = {
     networks: {
@@ -12,44 +7,15 @@ module.exports = {
        port: 8545,
        network_id: "*"
       },
-      kovan: {
-        provider: () => new HDWalletProvider({
-          mnemonic: {
-            phrase: mnemonic
-          },
-          providerOrUrl: `https://kovan.infura.io/v3/efc89509a2c24e41bb674076b0751fbd`,
-          chainId: "42"
-        }),
-        network_id: "42",
-        gas: 5500000
-      },
-      arbitrumRemote: {
-        provider: function () {
-          return wrapProvider(
-            new HDWalletProvider({
-              mnemonic: {
-                phrase: mnemonic
-              },
-              numberOfAddresses: 1,
-              providerOrUrl: 'https://kovan4.arbitrum.io/rpc',
-              shareNonce: true,
-              derivationPath: "m/44'/1'/0'/0/",
-              chainId: "212984383488152"
-            })
-          )
-        },
-        network_id: '*', // Match any network id
-        gasPrice: 0,
-      },
+      dashboard: {
+
+      }
     },
     compilers: {
       solc: {
-        version: "0.6.2",
+        version: "0.8.13",
       }
     },
-    contracts_build_directory: path.join(__dirname, 'artifacts'),
-    plugins: [
-      "truffle-plugin-debugger"
-    ]
+    contracts_build_directory: './src/contracts',
   }
   
