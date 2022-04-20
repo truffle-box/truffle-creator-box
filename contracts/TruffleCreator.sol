a743c95a765b9dd8ed648628f28eaa188f043ae2
Kevin Bluer <kevin@bluer.com>
2022-04-20 16:51:32 -0500

    tidying maybe

--- contracts/TruffleCreator.sol
+++ contracts/TruffleCreator.sol
@@ -1,30 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.0;
+pragma solidity 0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
 
-contract MFT is ERC721, ERC721Burnable {
+contract Remixr is ERC721URIStorage {
+    string baseURI;
+
     using Counters for Counters.Counter;
     Counters.Counter private _id;
 
-    constructor() ERC721("Truffle Creator", "TRFL") public {
-        _setBaseURI("ipfs://");
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+    function setTokenURI(
+        uint256 tokenId, 
+        string memory tokenURI
+    ) external {
+        _setTokenURI(tokenId, tokenURI);
+    }
+    
+    function setBaseURI(string memory baseURI_) external {
+        baseURI = baseURI_;
+    }
+    
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
     }
 
-    function mint(address owner, string memory cid) public returns (uint256) {
-
+    function mint(
+        address owner, 
+        string memory cid
+    ) external returns (uint256) {
         _id.increment();
-
         uint256 newId = _id.current();
+
         _mint(owner, newId);
         _setTokenURI(newId, cid);
 
         return newId;
     }
-
-    // function burnById(uint256 tokenId) public{
-    //     burn(tokenId);
-    // }
 }
